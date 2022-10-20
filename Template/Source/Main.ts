f8be2d143bf98d3ad4e4084e3579463bbf7b0c93
Sarah Franke <sarah.franke@hs-furtwangen.de>
2022-10-20 18:52:52 +0200

    set some code

--- Template/Source/Main.ts
+++ Template/Source/Main.ts
@@ -4,11 +4,65 @@ namespace Template {
 
   console.log("FudgeStory template starting");
 
+  export let transition = {
+
+    puzzle: {
+      duration: 1,
+      alpha: "",
+      edge: 1
+    }
+  };
+
+  export let sound = {
+    //background themes
+
+    //SFX
+    drop: ""
+  };
+
+  export let locations = {
+    beachDay: {
+      name: "beachDay",
+      background: "",
+
+    }
+  };
+
+  export let characters = {
+    narrator: {
+      name: ""
+    },
+    protagonist: {
+      name: ""
+    },
+    aisake: {
+      name: "Aisaka",
+      origin: ƒS.ORIGIN.BOTTOMCENTER,
+      pose: {
+        angry: "pfad des bildes",
+        happy: "",
+        upset: ""
+      }
+    }
+  };
+
+  //Data that will be saved (game progress)
+
+  export let dataForSave = {
+    nameProtagonist: ""
+  };
+
+
   window.addEventListener("load", start);
+
+
   function start(_event: Event): void {
     let scenes: ƒS.Scenes = [
       { scene: Scene, name: "Scene" }
     ];
+
+    let uiElement: HTMLElement = document.querySelector("[type=interface]");
+    dataForSave = ƒS.Progress.setData(dataForSave, uiElement);
 
     // start the sequence
     ƒS.Progress.go(scenes);
