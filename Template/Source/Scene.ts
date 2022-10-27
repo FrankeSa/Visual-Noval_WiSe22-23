e97dd6c7f2885a31d7c364b4212a2cf5a9c01a7b
Sarah Franke <sarah.franke@hs-furtwangen.de>
2022-10-27 18:21:20 +0200

    new code

--- Template/Source/Scene.ts
+++ Template/Source/Scene.ts
@@ -1,5 +1,26 @@
 namespace Template {
   export async function Scene(): ƒS.SceneReturn {
     console.log("FudgeStory Template Scene starting");
+
+    let text = {
+
+      Aisaka: {
+
+        text1: "Hi",
+        text2: "Hi",
+        text3: "Hi",
+
+      }
+    }
+
+    ƒS.Speech.hide();
+    await ƒS.Location.show(locations.beachDay);
+    await ƒS.Character.show(characters.aisake, characters.aisake.pose.happy, ƒS.positionPercent(100, 70))
+    await ƒS.update();
+
+
+
   }
+
+
 }
